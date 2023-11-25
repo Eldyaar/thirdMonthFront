1a5d6323e25f2742c544ac71e3d0495848d77cbe
Eldiyar Dolbekov <119499658+Eldyaar@users.noreply.github.com>
2023-11-25 23:45:14 +0300

    Update lesson.js
    Convertor .  Lesson 5

--- js/lesson.js
+++ js/lesson.js
@@ -27,6 +27,7 @@ window.addEventListener('DOMContentLoaded', () => {
             tabs.forEach((tabElement, tabIndex) => {
                 if (event.target === tabElement) {
                     hideTabsContent()
+                    currentTab = tabIndex
                     showTabsContent(tabIndex)
                 }
             })
@@ -44,4 +45,49 @@ window.addEventListener('DOMContentLoaded', () => {
     const tabInterval = setInterval(nextTab, 3000)
 
     // === / TABS ===
+
+     // === CONVERTOR ===
+
+    const somInput = document.querySelector('#som')
+    const usdInput = document.querySelector('#usd')
+    const rubInput = document.querySelector('#rub')
+
+    const convertor = (element, targetElement, moreTargetElement, type) => {
+        element.addEventListener('input', () => {
+            const req = new XMLHttpRequest()
+            req.open('GET', '../data/convertor.json')
+            req.setRequestHeader('Content-type', 'application/json')
+            req.send()
+            
+            req.onload = () => {
+                const changes = JSON.parse(req.response)
+
+                switch (type) {
+                    case 'som': 
+                        targetElement.value = (element.value / changes.usd).toFixed(2)
+                        moreTargetElement.value = (element.value / changes.rub).toFixed(2)
+                        break
+                    case 'usd':
+                        targetElement.value = (element.value * changes.usd).toFixed(2)
+                        moreTargetElement.value = (element.value * changes.usdToRub).toFixed(2)
+                        break
+                    case 'rub':
+                        targetElement.value = (element.value * changes.rub).toFixed(2)
+                        moreTargetElement.value = (element.value / changes.usd).toFixed(2)
+                        break
+                }
+
+                if (element.value === '') {
+                    targetElement.value = ''
+                    moreTargetElement.value = ''
+                }
+            }
+        })
+    }
+
+    convertor(somInput, usdInput, rubInput, 'som')
+    convertor(usdInput, somInput, rubInput, 'usd')
+    convertor(rubInput, somInput, usdInput, 'rub')
+
+    // === / CONVERTOR ===
 })
